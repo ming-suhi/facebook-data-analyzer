14d645066505e3aa63d5524fc9ccabab966f79db
明suhi <k.j.b.taton@gmail.com>
2021-10-20 14:13:58 +0800

    Feature: Add script for table datas

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -31,3 +31,51 @@ fetchRoute('/user/messages/sent-per-hour/chart-data', data => {
   document.getElementById('hourly-chart').appendChild(canvas);
   new Chart(canvas, data);
 });
+
+fetchRoute('/user/channels/rankedByMessages', data => {
+  const table = document.createElement("table");
+  const thead = table.createTHead();
+  thead.innerHTML = `
+  <tr>
+    <th scope="col">#</th>
+    <th scope="col">Channel</th>
+    <th scope="col">Your Messages Count</th>
+  </tr>
+  `
+  const tbody = table.createTBody();
+  for(let channelRank in data.slice(0, 10)) {
+    const channel = data[channelRank];
+    const row = document.createElement('tr');
+    row.innerHTML = `
+    <th><strong>${parseInt(channelRank) + 1}</strong></th>
+    <td>${channel.name.length > 25 ? channel.name.substring(0, 25) + "..." : channel.name}</td>
+    <td>${channel.count}</td>
+    `
+    tbody.appendChild(row);
+  }
+  document.getElementById('channels-table').appendChild(table);
+});
+
+fetchRoute('/user/words/occurences', data => {
+  const table = document.createElement("table");
+  const thead = table.createTHead();
+  thead.innerHTML = `
+  <tr>
+    <th scope="col">#</th>
+    <th scope="col">Words</th>
+    <th scope="col">Occurences</th>
+  </tr>
+  `
+  const tbody = table.createTBody();
+  for(let wordRank in data.slice(0, 10)) {
+    const word = data[wordRank];
+    const row = document.createElement('tr');
+    row.innerHTML = `
+    <th><strong>${parseInt(wordRank) + 1}</strong></th>
+    <td>${word.name.length > 25 ? word.name.substring(0, 25) + "..." : word.name}</td>
+    <td>${word.count}</td>
+    `
+    tbody.appendChild(row);
+  }
+  document.getElementById('words-table').appendChild(table);
+});
