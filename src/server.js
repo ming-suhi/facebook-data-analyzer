5d5bc5039cedabf1670f3e3eefce1c8395eb022b
明suhi <k.j.b.taton@gmail.com>
2021-11-10 17:59:17 +0800

    Feature: Refactor CSS

--- src/server.js
+++ src/server.js
@@ -3,19 +3,14 @@ const dotenv = require('dotenv');
 const messagesRouter = require('./routes/messages');
 const channelsRouter = require('./routes/channels');
 const wordsRouter = require('./routes/words');
-const Client = require('./client');
 
 // Set env variables
 dotenv.config();
-
-// Create client
-const client = new Client();
 
 // Create express app
 const app = express();
 
 // Set views directory to current directoy
-app.set('views', __dirname);
 app.use(express.json());
 app.use(express.static(__dirname));
 
