0b9e578627b5523617f2b7058db6db8fbe2cf0df
明suhi <k.j.b.taton@gmail.com>
2021-11-30 13:50:18 +0800

    Feature: Remove MainContainer and CountSection components
    - Removed MainContainer and CountSection components
    - Refactored CountSection content to MainContainer
    - Refactored MainContainer content to MainPage
    - Localized MainContainer styles to MainPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,33 @@
-import MainContainer from "./components/MainContainer";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Count from "./components/Count";
+import GraphSkeleton from "./components/GraphSkeleton";
+import Table from "./components/Table";
 
 function App() {
   return (
     <div id="main-page" grid="" background="" theme-color="dark">
       <Header />
-      <MainContainer />
+      <main grid="" id="main-container">
+        <section flex="left space-evenly" text="center" container="" edge="smooth">
+          <Count route="/user/messages/encountered" label="messages encounter" />
+          <Count route="/user/messages/received" label="messages received" />
+          <Count route="/user/messages/sent" label="messages sent" />
+          <Count route="/user/words/sent" label="words sent" />
+        </section>
+        <section flex="top center-x center-y" container="" edge="smooth" id="yearly-chart">
+          <GraphSkeleton />
+        </section>
+        <section flex="top center-x center-y" container="" edge="smooth" id="hourly-chart">
+          <GraphSkeleton />
+        </section>
+        <section flex="top center-x center-y" container="" edge="smooth" id="channels-table">
+          <Table route="/user/channels/rankedByMessages" labels={["#", "Channel", "Your Messages Count"]} />
+        </section>
+        <section flex="top center-x center-y" container="" edge="smooth" id="words-table">
+          <Table route="/user/words/occurences" labels={["#", "Words", "Occurences"]} />
+        </section>
+      </main>
       <Footer />
     </div>
   );
