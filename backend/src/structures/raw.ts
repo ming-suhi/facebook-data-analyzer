91221d1d6df3877519871c318005433735f510cb
明suhi <k.j.b.taton@gmail.com>
2021-12-02 18:17:24 +0800

    Feature(Types): Created types for raw object
    - Created rawMessage, rawParticipant, and rawChannel

---
+++ backend/src/structures/raw.ts
@@ -0,0 +1,57 @@
+/**
+ * Message object from json file.
+ * 
+ * @remarks
+ * Only used elements from object is type documented.
+ */
+export type rawMessage = {
+  /** 
+   * The name of the sender 
+   */
+  sender_name: string,
+  /** 
+   * The time the message was sent 
+   */
+  timestamp_ms: number,
+  /** 
+   * The content of the message 
+   */
+  content: string
+}
+
+
+/** 
+ * The participant object from json file.
+ */
+export type rawParticipant = {
+  /** 
+   * The name of the participant 
+   */
+  name: string
+}
+
+
+/**
+ * The channel object from json file.
+ * 
+ * @remarks
+ * Only used elements from object is type documented.
+ */
+ export type rawChannel = {
+  /**
+   * The name of the channel
+   */
+  title: string,
+  /**
+   * The type of the channel
+   */
+  thread_type: "Regular" | "RegularGroup",
+  /**
+   * The participants of the channel
+   */
+  participants: rawParticipant[],
+  /**
+   * All messages sent in the channel
+   */
+  messages: rawMessage[]
+}
